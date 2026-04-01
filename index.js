81db8291002bf24c1590eb39aa54aecd2b28865b
WhiteTafal <givijf@gmail.com>
2026-04-01 19:59:54 +0400

    set up vite and env variables

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtvtvbbFoyK11uQHsyI4dmuHp-wWi3Jec",
-  authDomain: "realtime-database-8ec8d.firebaseapp.com",
-  databaseURL: "https://realtime-database-8ec8d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "realtime-database-8ec8d",
-  storageBucket: "realtime-database-8ec8d.appspot.com",
-  messagingSenderId: "414125472687",
-  appId: "1:414125472687:web:740bdd6033342ccd9bcd5a"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig)
