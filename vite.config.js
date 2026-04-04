cbd2e302be9b1a21c488a8b7b43fd2f9f206a50b
WhiteTafal <givijf@gmail.com>
2026-04-04 20:31:54 +0400

    Move static assets to public folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,29 @@
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: []	
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Reminder Notes App',
+                short_name: 'Reminders',
+                theme_color: '#AC485A',
+                background_color: '#EEF0F4',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: '/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
 })
